3cea90b71442dede991a9819550f5532b917af3b
warglaivee <warglaivegm@gmail.com>
2017-04-14 21:13:44 +0300

    PC Update

---
+++ 01. Placeholders/01. Placeholders.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Placeholders
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            var input = Console.ReadLine();
+            while (input != "end") 
+            {
+                var someString = input
+                    .Split(new string[] { "->" }
+                    ,StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                var sentence = someString[0];
+                var elements = someString[1]
+                    .Split(new[] { ' ', ',' }
+                , StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+                for (int i = 0; i < elements.Count; i++)
+                {
+                    var placeHolderFormat = "{" + i + "}";
+                    sentence = sentence.Replace(placeHolderFormat, elements[i]);
+                }
+                Console.WriteLine(sentence);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 02. JSON Stringify/02. JSON Stringify.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.JSON_Stringify
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public List<string> Grades { get; set; }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            var result = new List<Student>();
+            var input = Console.ReadLine();
+            while (input != "stringify")
+            {
+                var token = input
+                    .Split(new[] { ' ', ':', '-', '>', ',' }
+                , StringSplitOptions.RemoveEmptyEntries);
+                var name = token[0];
+                var age = token[1];
+                var grades = token.Skip(2).ToList();
+                var newStudents = new Student
+                {
+                    Name = name,
+                    Age = age,
+                    Grades = grades
+                };
+                result.Add(newStudents);
+
+                input = Console.ReadLine();
+            }
+            string output = string.Empty;
+            output += "[";
+            for (int i = 0; i < result.Count; i++)
+            {
+                Student currentStudent = result[i];
+                output += "{";
+                output += "name:\"" + currentStudent.Name + "\"" + ",";
+                output += "age:" + currentStudent.Age + ",";
+                output += "grades:[" + string.Join(", ", currentStudent.Grades) + "]";
+                output += "}";
+                if (i < result.Count - 1) 
+                {
+                    output += ',';
+                }
+            }
+            output += "]";
+            Console.WriteLine(output);
+        }
+    }
+}
