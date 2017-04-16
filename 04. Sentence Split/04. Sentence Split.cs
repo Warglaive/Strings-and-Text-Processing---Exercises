03199596f4f312c40245d28e8528fbd1ab51f94d
warglaivee <warglaivegm@gmail.com>
2017-04-16 21:59:17 +0300

    04.Sentence split written
    05. Capitalize Words written

---
+++ 04. Sentence Split/04. Sentence Split.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04.Sentence_Split
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var sentence = Console.ReadLine();
+            var delimiter = Console.ReadLine();
+            var splitted = sentence.Split(new string[] { delimiter }, StringSplitOptions.None);
+            Console.WriteLine("[" + string.Join(", ", splitted) + "]");
+        }
+    }
+}
---
+++ 05. Capitalize Words/05. Capitalize Words.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Capitalize_Words
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            var sentence = Console.ReadLine();
+            while (sentence!="end")
+            {
+                var result = new List<string>();
+                var token = sentence.ToLower()
+                    .Split(new[] { ' ', ',', '.', ':', ';', '[', ']', '{', '}', '"', '\\', '(', ')', '-', '=', '>', '<', '~', '|', '`', '?', '!', '/', '*', '%', '$', '#', '@', '^', '&' }
+                , StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                for (int i = 0; i < token.Count; i++)
+                {
+                    if (token[i] != string.Empty)
+                    {
+                        token[i] = UppercaseFirst(token[i]);
+                        result.Add(token[i]);
+                    }
+                }
+                Console.WriteLine(string.Join(", ", result));
+                sentence = Console.ReadLine();
+            }
+        }
+        static string UppercaseFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            char[] a = s.ToCharArray();
+            a[0] = char.ToUpper(a[0]);
+            return new string(a);
+        }
+    }
+}
